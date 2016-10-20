6f6133f85a8e6fb3e9ffe76243af5e6d468f7133
Ranjithprabhu K <ranjithprabhu.k@gmail.com>
2016-10-20 12:37:01 +0530

    Initial Working Version
    Initial Working Version Committed

---
+++ README.md
@@ -0,0 +1,31 @@
+# Angular Image Viewer
+Angular Imager Viewer is a angular directive for image zoom , image rotate feautured in HTML pages.
+Image Zoom in, Zoom out, Image Rotate left, Image Rotate right options were added in this directive.
+
+## Get The Dashboard
+
+* [Demo] (http://dashboard.ranjithprabhu.in)
+
+* [Download Source]  (https://github.com/ranjithprabhuk/Angular-Image-Viewer/archive/master.zip)
+
+
+## Features
+* Image Zoom In
+* Image Zoom Out
+* Image Rotate Left
+* Image Rotate Right
+
+## Plugins used
+* Bootstrap for CSS styles and Glyphicons
+
+## Usage
+* Download and customize the application as per your requirements.
+
+## About Author
+* [Author URL] (http://ranjithprabhu.in)
+
+I am passionate in playing with pixels, creating attractive designs which interact well with the user and love developing web apps. Have a good background in web design and development. Also having wonderful working experience with various interesting projects and participated in the development of the products to provide end to end solutions.
+
+
+## License
+Released under the MIT license.
---
+++ dist/image-viewer.js
@@ -0,0 +1,203 @@
+/**
+ * @Angular Image Vieer Directive
+ *
+ * @Author 			: Ranjithprabhu K
+ *
+ * @Module Name 	: 	imageViewer
+ *
+ * @Directive Name	:	image-viewer [in DOM]
+ *
+ * @Description:  This handles the image viewer with image zoom-in, zoom-out, rotate-left, rotate-right options.
+ *
+ */
+
+(function () {
+console.log("coming");
+'use strict';
+angular.module('imageViewer',[]).directive('imageViewer',function(){
+
+     return {
+         restrict: 'EA',
+         scope:{
+             closeCanvas: '&'
+         },
+         template:'<div style="padding:10px;">\
+                <button id="girardir" class="btn btn-primary btn-sm" ng-disabled="noImage"><span class="fa fa-rotate-left"> </span> Rotate to Left</button>\
+                <button id="giraresq" class="btn btn-primary btn-sm" ng-disabled="noImage"><span class="fa fa-rotate-right"> </span> Rotate to Right</button>\
+                <button id="zoomIn" class="btn btn-info btn-sm" ng-disabled="noImage"><span class="fa fa-plus"> </span> Zoom</button>\
+                <button id="zoomOut" class="btn btn-info btn-sm" ng-disabled="noImage"><span class="fa fa-minus"> </span> Zoom</button>\
+                <button id="carregar" class="btn btn-warning btn-sm" ng-disabled="noImage"><span class="fa fa-arrow-left"> </span> Reset</button>\
+                <button id="close" class="btn btn-danger btn-sm pull-right"><span class="fa fa-close"> </span> Close</button>\
+                </div>\
+				<div id="image-zoom" style="padding:10px;">\
+				<h2 class="text-center text-danger" id="error-message" style="border:1px solid #000;display:none;padding:20px">Image Not Available</h2>\
+                <canvas id="canvas" height="650" data-girar="0" style="border:1px solid #000;z-index:555555;cursor:grab;">sdsd</canvas>\
+                <img ng-src="{{image}}" id="image" style="display:none" /> </div>',
+         link: function(scope,element,attr){
+             scope.image = attr.src;
+			 scope.noImage = false;
+
+//options for the zoom and rotate 
+var canvas = document.getElementById('canvas');
+var image = document.getElementById('image');
+var element = canvas.getContext("2d");
+var angleInDegrees = 0;
+var zoomDelta = 0.1;
+var currentScale = 1;
+var currentAngle = 0;
+var canvasWidth = 600;
+var novosDadosTRBL;
+var novosDadosWH;
+var novosDadosW;
+var novosDadosH;
+var startX, startY, isDown = false;
+scope.flag=1;
+
+
+
+
+//set the width of the canvas
+setTimeout(function(){
+    canvas.width = angular.element('#image-zoom').width();
+
+    //append the image in canvas
+    document.getElementById('carregar').click();
+},0);
+
+//method to reset the image to its original position
+angular.element('#carregar').click(function () {
+	
+	//check the image load
+	angular.element('#image').on('load',function(){
+		//load the  image in canvas if the image is loaded successfully
+		image = document.getElementById('image');
+		element = canvas.getContext("2d");
+		angleInDegrees = 0;
+		currentScale = 1;
+		currentAngle = 0;
+		if(scope.flag){
+			scope.flag = 0;
+			element.translate(canvas.width / 2, canvas.height / 2);
+		}
+		else
+			element.translate(0,0);
+		drawImage();
+		angular.element('#canvas').attr('data-girar', 0);
+		
+		console.log('loaded successfully!!!');})
+	
+	//if the image is not loaded 
+	.on('error', function() { 
+		//hide the canvas
+		angular.element('#canvas').hide();
+		
+		//disable the buttons
+		scope.noImage = true;
+		
+		//display the image not loaded text
+		angular.element('#error-message').show();
+		console.log("error loading image"); });
+    
+});
+
+//method to rotate the image in clockwise
+angular.element('#giraresq').click(function () {
+    angleInDegrees = 5;
+    currentAngle += angleInDegrees;
+    drawImage();
+});
+
+
+//method to rotate the image in anti clockwise
+angular.element('#girardir').click(function () {
+    angleInDegrees = -5;
+    currentAngle += angleInDegrees;
+    drawImage();
+});
+
+
+//method to zoom in the image
+angular.element('#zoomIn').click(function () {
+    currentScale += zoomDelta;
+    drawImage();
+});
+
+
+//method to zoom in and zoom out the image on mouse wheel scroll
+angular.element('#canvas').bind('mousewheel DOMMouseScroll', function(event){
+    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+        // scroll up
+         currentScale += zoomDelta;
+        drawImage();
+    }
+    else {
+        // scroll down
+        if(currentScale-zoomDelta - 0.1 > 0){
+        currentScale -= zoomDelta;
+         drawImage();
+         }
+    }
+});
+
+//method to zoom out the image
+angular.element('#zoomOut').click(function () {
+    currentScale -= zoomDelta;
+    drawImage();
+});
+
+//method to close the dialog box
+angular.element('#close').click(function () {
+    scope.closeCanvas();
+   //clear();
+});
+
+canvas.onmousedown = function (e) {
+    var pos = getMousePos(canvas, e);
+    startX = pos.x;
+    startY = pos.y;
+    isDown = true;
+}
+
+canvas.onmousemove = function (e) {
+    if (isDown === true) {
+        var pos = getMousePos(canvas, e);
+        var x = pos.x;
+        var y = pos.y;
+
+        element.translate(x - startX, y - startY);
+        drawImage();
+        
+        startX = x;
+        startY = y;
+    }
+}
+window.onmouseup = function (e) {
+    isDown = false;
+}
+
+function drawImage() {
+    clear();
+    element.save();
+    element.scale(currentScale, currentScale);
+    element.rotate(currentAngle * Math.PI / 180);
+    element.drawImage(image, -image.width / 2, -image.height / 2);
+    element.restore();
+}
+
+function clear() {
+    element.clearRect(-2000,-2000,5000,5000);
+}
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+
+         }
+     }
+ });
+ 
+})();
